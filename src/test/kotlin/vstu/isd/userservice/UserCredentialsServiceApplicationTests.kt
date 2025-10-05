d84994f32328ce0562d7c4aebe64ccd4b17cff8f
sauce-chili <saucechilidev@gmail.com>
2025-10-05 00:38:41 +0000

    feat: added load context test

---
+++ src/test/kotlin/vstu/isd/userservice/UserCredentialsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package vstu.isd.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserCredentialsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
