78d520614be3650d38b793a016e6a005176f75ae
Vacobi <roman.yurasov.v@gmail.com>
2025-10-12 21:07:50 +0000

    fix: fixed security filter chain to allow access to Swagger

--- src/main/kotlin/vstu/isd/userservice/config/SecurityRequestsConfig.kt
+++ src/main/kotlin/vstu/isd/userservice/config/SecurityRequestsConfig.kt
@@ -2,26 +2,25 @@ package vstu.isd.userservice.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import java.lang.Exception
 
 @Configuration
 @EnableWebSecurity
 class SecurityRequestsConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+
         http
+            .csrf().disable()
             .cors().and()
-            .csrf().disable() // TODO fix it
-            .authorizeHttpRequests { authorizeHttpRequests ->
-                authorizeHttpRequests
-                    .requestMatchers("/api/v1/auth/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/v1/user/register").permitAll()
-                    .anyRequest().authenticated()
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                requests.anyRequest().authenticated()
             }
         return http.build()
     }
+
 }
